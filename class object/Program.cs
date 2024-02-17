4b9d4c345a7813d578e8937f46e02acccf8da45f
ayush <ayush@AYUSHGANDHI>
2024-02-17 11:27:05 +0530

    Add project files.

---
+++ class object/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student information = new Student();
+
+            information.Input();
+            information.Output();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ class object/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_object
+{
+    public class Student
+    {
+        public int Id = 0;
+        public string Name = "";
+        public int sub1 = 0;
+        public int sub2 = 0;
+        
+
+        public void Input()
+        {
+            Console.WriteLine("Please Enter ID");
+            Id=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Name");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Please Enter Marks of Sub1:");
+            sub1=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Marks of Sub2:");
+            sub2=Convert.ToInt32(Console.ReadLine());
+        }
+        public void Output() 
+        {
+            Console.WriteLine(Id);
+            Console.WriteLine(Name);
+            Console.WriteLine(sub1+sub2);
+        }
+    }
+}
